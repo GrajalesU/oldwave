b3d90756f31659dfe894a4e4e249e08cf84aff38
Maria Rua <maria.rua1@udea.edu.co>
2022-08-12 14:23:49 -0500

    Create state for search-bar funcionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import './App.css'
-import ProductPage from './pages/ProductPage/ProductPage'
+import React from "react";
+import "./App.css";
+import ProductPage from "./pages/ProductPage/ProductPage";
 
-    import { useState } from "react";
-    import Header from "./components/Header/Header";
-    import SearchBar from "./components/SearchBar/SearchBar";
-    
-    import "./styles/main.css";
-    
-    function App() {
-      const [count, setCount] = useState(0);
-      
-      return (
-        <div>
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import SearchBar from "./components/SearchBar/SearchBar";
+import "./styles/main.css";
+import { useEffect } from "react";
+
+function App() {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (query) => {
+    console.log(query);
+    setSearch(query);
+    fetch("https://oldwave-backend.herokuapp.com/api/product?query=" + query)
+      .then((response) => response.json())
+      .then((data) => console.log(data, search));
+  };
+
+  return (
+    <div>
       <Header></Header>
-      <SearchBar></SearchBar>
-      <ProductPage/>
+      <SearchBar onSearch={handleSearch} />
+      <ProductPage />
     </div>
   );
 }
