181aecd28c2f007a7ef74c6760547a2e37149ec1
Maria Rua <maria.rua1@udea.edu.co>
2022-08-12 18:40:55 -0500

    implement useNavigate for redirect onSearch

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,17 @@
 import Header from "../Header/Header";
+import SearchBar from "../SearchBar/SearchBar";
+import { useNavigate, useParams } from "react-router";
 
 function Layout({ children }) {
+  const navigate = useNavigate();
+  const handleSearch = (query) => {
+    navigate("/search/" + query);
+  };
+
   return (
     <div>
       <Header />
+      <SearchBar onSearch={handleSearch} />
       {children}
     </div>
   );
