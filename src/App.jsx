eccf9076cc4d43eefac0b16fdc083561bb80cff5
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-20 10:38:43 -0500

    Add user context

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,24 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Login from "./pages/Login/Login";
+import { UserProvider } from "./context/user";
 
 function App() {
   return (
     <GoogleOAuthProvider clientId="386602516499-pt5f8m9f2dm57lisi3utvtdpdl7s6fhh.apps.googleusercontent.com">
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/search/:query" element={<Search />} />
-            <Route path="/product/:id/*" element={<ProductDetail />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/search/:query" element={<Search />} />
+              <Route path="/product/:id/*" element={<ProductDetail />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </UserProvider>
     </GoogleOAuthProvider>
   );
 }
