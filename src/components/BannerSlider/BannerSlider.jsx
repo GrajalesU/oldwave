197c67d20659dbd898c7509a9380d5ad54345aec
Maria Rua <maria.rua1@udea.edu.co>
2022-10-20 11:39:36 -0500

    BannerSlider component improvements and test file creation

--- src/components/BannerSlider/BannerSlider.jsx
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -1,28 +1,25 @@
 import Slider from "react-slick";
-import React, { Component } from "react";
-import Banner1 from "../../assets/images/banner-blackfriday.png";
-import Banner2 from "../../assets/images/banner-auxilar-ordenadores.png";
-import Banner3 from "../../assets/images/banner-auxiliar-bicicletas.png";
 
-export default class BannerSlider extends Component {
-  render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
-    return (
+function BannerSlider({ images, infinite = true }) {
+  const settings = {
+    dots: true,
+    infinite,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div>
       <div>
-        <div>
-          <Slider {...settings}>
-            <img alt="Imagen de promoción por black-friday" src={Banner1} />
-            <img alt="Imagen de promoción por black-friday" src={Banner2} />
-            <img alt="Imagen de promoción por black-friday" src={Banner3} />
-          </Slider>
-        </div>
+        <Slider {...settings}>
+          {images.map((image) => (
+            <img key={image.url} alt={image.title} src={image.url} />
+          ))}
+        </Slider>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default BannerSlider;
