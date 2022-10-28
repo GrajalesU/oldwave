4fead8bc722f4c022e6d794e0a5e6d761b009478
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-28 16:14:32 -0500

    Set user to db

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,3 +9,36 @@ export const getProduct = async (id) => {
     return null;
   }
 };
+
+export const handleUser = async (sub, email, name, lastname) => {
+  const response = await fetch(
+    "https://oldwave-backend.herokuapp.com/api/users/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sub,
+        email,
+        name,
+        lastname,
+      }),
+    }
+  );
+
+  const result = await response.json();
+  return result;
+};
+
+// const prueba = async () => {
+//   const sub = "103041095737993934933";
+//   const email = "juan.grajalesu@udea.edu.co";
+//   const name = "JUAN MANUEL";
+//   const lastName = "GRAJALES URQUIJO";
+
+//   const result = await createUser(sub, email, name, lastName);
+//   console.log(result);
+// };
+
+// await prueba();
