e50a68e53df74b5d51e453fde297bc23c8c41b36
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-28 19:53:12 -0500

    Add Orders

--- src/components/OrderCard/OrderCard.jsx
+++ src/components/OrderCard/OrderCard.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import styles from "./OrderCard.module.css";
-import GoogleLogo from "../../assets/google-logo.png";
-export default function OrderCard() {
+export default function OrderCard({ img, description, reseller, date }) {
+  const currentDate = new Date(date);
+  const formatDate = currentDate.toLocaleDateString("es-ES", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <div className={styles.product}>
-      <h3 className={styles.productCreationDate}>17 de Noviembre del 2021</h3>
+      <h3 className={styles.productCreationDate}>{formatDate}</h3>
       <div className={styles.productDetail}>
-        <img
-          src={GoogleLogo}
-          alt="product"
-          className={styles.productThumbnail}
-        />
+        <img src={img} alt="product" className={styles.productThumbnail} />
         <div className={styles.productOrder}>
           <small className={styles.productOrderSend}>Entregado</small>
           <span className={styles.productOrderReceivedDate}>
-            Llegó el 20 de Noviembre
+            Llegó el {formatDate}
           </span>
-          <p className={styles.productOrderDescription}>
-            Descripción del producto, lorem ipsum porro est qui ipsum quia dolor
-            sit, consectetur, ...
-          </p>
+          <p className={styles.productOrderDescription}>{description}</p>
         </div>
         <div className={styles.reseller}>
-          <h4 className={styles.resellerName}>Juliana Rojas Arroyare</h4>
+          <h4 className={styles.resellerName}>{reseller}</h4>
           <a href="" className={styles.resellerContact}>
             Enviar mensaje
           </a>
