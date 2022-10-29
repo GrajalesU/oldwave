3d41e57ee9f803ffb707f2ce8b6cdca0dc486a17
Maria Rua <maria.rua1@udea.edu.co>
2022-10-28 20:57:54 -0500

    Add home items redirection and tests

--- src/components/BannerSlider/BannerSlider.test.jsx
+++ src/components/BannerSlider/BannerSlider.test.jsx
@@ -1,33 +1,69 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import BannerSlider from "./BannerSlider";
+import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 const IMAGES_MOCK = [
   {
+    name: "mockImage1",
     title: "mockImageTitle1",
     url: "mockImageUrl1",
   },
   {
+    name: "mockImage2",
     title: "mockImageTitle2",
     url: "mockImageUrl2",
   },
   {
+    name: "mockImage3",
     title: "mockImageTitle3",
     url: "mockImageUrl3",
   },
 ];
 
+const mockNavigateFn = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigateFn,
+}));
+
 describe("Banner Slider Component", () => {
   test("should render correctly when its not infinite", () => {
-    render(<BannerSlider images={IMAGES_MOCK} infinite={false} />);
+    render(
+      <MemoryRouter>
+        <BannerSlider images={IMAGES_MOCK} infinite={false} />
+      </MemoryRouter>
+    );
     expect(screen.getByAltText(IMAGES_MOCK[0].title)).toBeInTheDocument();
   });
 
   test("should render correctly when its infinite", () => {
-    render(<BannerSlider images={IMAGES_MOCK} />);
+    render(
+      <MemoryRouter>
+        <BannerSlider images={IMAGES_MOCK} />
+      </MemoryRouter>
+    );
     const imageComponent = screen.getAllByAltText(IMAGES_MOCK[0].title)[0];
     expect(imageComponent).toBeInTheDocument();
     expect(imageComponent).toHaveAttribute("src", IMAGES_MOCK[0].url);
+  });
+
+  test("should go to product when clicks image", async () => {
+    render(
+      <MemoryRouter>
+        <BannerSlider images={IMAGES_MOCK} />
+      </MemoryRouter>
+    );
+    const imageComponent = screen.getAllByAltText(IMAGES_MOCK[0].title)[0];
+    expect(imageComponent).toBeInTheDocument();
+    expect(imageComponent).toHaveAttribute("src", IMAGES_MOCK[0].url);
+    userEvent.click(imageComponent);
+    await waitFor(() =>
+      expect(mockNavigateFn).toHaveBeenCalledWith(
+        `/search/${IMAGES_MOCK[0].name}`
+      )
+    );
   });
 });
--- src/components/CategoriesCarousel/CategoriesCarousel.test.jsx
+++ src/components/CategoriesCarousel/CategoriesCarousel.test.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import CategoriesCarousel from "./CategoriesCarousel";
+import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 const ICONS_MOCK = [
   {
@@ -22,15 +24,45 @@ const ICONS_MOCK = [
   },
 ];
 
+const mockNavigateFn = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigateFn,
+}));
+
 describe("Categories Carrousel Component", () => {
   test("should render correctly when its not infinite", () => {
-    render(<CategoriesCarousel icons={ICONS_MOCK} infinite={false} />);
+    render(
+      <MemoryRouter>
+        <CategoriesCarousel icons={ICONS_MOCK} infinite={false} />
+      </MemoryRouter>
+    );
     expect(screen.getByText(ICONS_MOCK[0].category)).toBeInTheDocument();
   });
 
   test("should render correctly when its infinite", () => {
-    render(<CategoriesCarousel icons={ICONS_MOCK} />);
+    render(
+      <MemoryRouter>
+        <CategoriesCarousel icons={ICONS_MOCK} />
+      </MemoryRouter>
+    );
     const imageComponent = screen.getAllByText(ICONS_MOCK[0].category)[0];
     expect(imageComponent).toBeInTheDocument();
+  });
+
+  test("should go to category when clicks icon", async () => {
+    render(
+      <MemoryRouter>
+        <CategoriesCarousel icons={ICONS_MOCK} />
+      </MemoryRouter>
+    );
+    const imageComponent = screen.getAllByText(ICONS_MOCK[0].category)[0];
+    expect(imageComponent).toBeInTheDocument();
+    userEvent.click(imageComponent);
+    await waitFor(() =>
+      expect(mockNavigateFn).toHaveBeenCalledWith(
+        `/search/${ICONS_MOCK[0].category}`
+      )
+    );
   });
 });
