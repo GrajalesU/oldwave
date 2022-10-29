2dd20404cf8f4a180dc1f9d564263c028898cf3f
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-29 07:48:33 -0500

    hotfix/ Add cypress script

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,11 @@
 Cypress.Commands.add("login", () => {
-  cy.request({
-    method: "GET",
-    url: "https://www.googleapis.com/oauth2/v3/userinfo?access_token=ya29.a0Aa4xrXP3-XyY_1-hF5cCLofSAk21-jsd-If7Mjj-nfRChTrwxLSVGyjrZFQTHo5QVx8apsDx89cozjt7s-9ZYzQtPiIQA9Ym78zpKF9LHN_CfNAnoHnvqj87hWpNBb-K7H17XjCdC9zjBiVPQS7dyZ1nh4yaDewaCgYKATASARASFQEjDvL9B9h8EX_0U6eJbfeUhAwp5w0166",
-  }).then(({ body }) => {
-    const user = {
-      name: body.name,
-      picture: body.picture,
-      sub: body.sub,
-      email: body.email,
-    };
+  const user = {
+    email: "juan.grajalesu@udea.edu.co",
+    name: "JUAN MANUEL GRAJALES URQUIJO",
+    picture:
+      "https://lh3.googleusercontent.com/a/ALm5wu3U5sBUH3HEP7Z9o-ZcQTeRTBHFU8R_doGHrBMbRw=s96-c",
+    sub: "103041095737993934933",
+  };
 
-    window.localStorage.setItem("user", JSON.stringify(user));
-  });
+  window.localStorage.setItem("user", JSON.stringify(user));
 });
