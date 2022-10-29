a9bc11c5a28fd1fdd0288d4f55321bf75a6dc996
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-28 22:24:16 -0500

    Add tests

--- src/components/SideBarShop/SideBarShop.test.jsx
+++ src/components/SideBarShop/SideBarShop.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import SideBarShop from "./SideBarShop";
+import { BrowserRouter } from "react-router-dom";
 
 const mockIncrementItem = jest.fn();
 const mockDecrementItem = jest.fn();
@@ -14,6 +15,7 @@ jest.mock("use-shopping-cart", () => {
         incrementItem: mockIncrementItem,
         decrementItem: mockDecrementItem,
         removeItem: mockRemoveItem,
+        clearCart: jest.fn(),
         cartDetails: {
           1: {
             key: 1,
@@ -30,25 +32,56 @@ jest.mock("use-shopping-cart", () => {
   };
 });
 
+jest.mock("../../context/user", () => {
+  return {
+    useUser: jest.fn(() => {
+      return {
+        name: "",
+        picture: "",
+        email: "",
+      };
+    }),
+    useDispatch: jest.fn(() => {
+      return jest.fn();
+    }),
+  };
+});
+
+jest.mock("../../utils/api", () => {
+  return {
+    addOrder: jest.fn(() => {
+      return {
+        Succes: true,
+      };
+    }),
+  };
+});
+
 const activeFn = jest.fn();
 
 describe("SideBarShop", () => {
   test("should render correctly", async () => {
-    render(<SideBarShop active={activeFn} />);
+    render(<SideBarShop active={activeFn} />, { wrapper: BrowserRouter });
     expect(screen.getByText("Carrito de compras")).toBeInTheDocument();
     expect(screen.getByText("FINALIZAR PEDIDO")).toBeInTheDocument();
   });
 
   test("should render products", async () => {
-    render(<SideBarShop active={activeFn} />);
+    render(<SideBarShop active={activeFn} />, { wrapper: BrowserRouter });
     expect(screen.getByText("mockProduct1")).toBeInTheDocument();
   });
 
   test("should close the sidebar when clicks close button", async () => {
-    render(<SideBarShop active={activeFn} />);
+    render(<SideBarShop active={activeFn} />, { wrapper: BrowserRouter });
     const closeButton = screen.getByTestId("close");
     expect(closeButton).toBeInTheDocument();
     fireEvent.click(closeButton);
     await waitFor(() => expect(activeFn).toHaveBeenCalled());
+  });
+
+  test("should end purchase when clicks end of purchase button", () => {
+    render(<SideBarShop active={activeFn} />, { wrapper: BrowserRouter });
+    const eofPurchase = screen.getByText("FINALIZAR PEDIDO");
+    fireEvent.click(eofPurchase);
   });
 });
